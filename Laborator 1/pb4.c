ad0d8906469102278cff65919389a00919ddba2d
Dan Nasta <dan@belphegor.tech>
2018-03-03 22:39:23 +0200

    Minor typo fixed

--- Laborator 1/pb4.c
+++ Laborator 1/pb4.c
@@ -9,10 +9,10 @@ void main(void)
     if (a == 0)
     {
         if (b == 0)
-            printf("X can be any real number");
+            printf("x can be any real number");
         else
             printf("Impossible as %f is not equal to zero", b);
     }
     else
-        print("X is %f", -b / a);
+        printf("x is %f", -b / a);
 }
