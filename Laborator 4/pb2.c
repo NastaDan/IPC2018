d8af3efcfa8290d89a8ac40d73bd8a6293671134
Dan Nasta <dan@belphegor.tech>
2018-03-13 00:21:57 +0200

    Initial Commit | Same task as Lab 3 problem 2

---
+++ Laborator 4/pb2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main(void)
+{
+    int n;
+    long long z = 2;
+    printf("n= ");
+    scanf("%i", &n);
+    printf("Prime factors of %i are: ", n);
+    while (z * z <= n)
+    {
+        if (n % z == 0)
+        {
+            printf("%i ", z);
+            n = n / z;
+            if (n == 1)
+                break;
+        }
+        else
+            z++;
+    }
+    if (n > 1)
+        printf("%i", n);
+}
